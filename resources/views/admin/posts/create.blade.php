c824345d03090bd4ac3a041e45dae2f0ec262fd6
BojanCacic <bojan.cacic.prijedor@gmail.com>
2020-02-10 20:18:00 +0100

    Post form

---
+++ resources/views/admin/posts/create.blade.php
@@ -0,0 +1,35 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-body">
+                    <h1 class="card-title">Create a new post</h1>
+
+                    <form action="/post/store" method="post">
+                        {{csrf_field()}}
+                        <div class="form-group">
+                            <label for="title">Title</label>
+                            <input type="text" class="form-control" name="title">
+                        </div>
+                        <div class="form-group">
+                            <label for="featured">Featured image</label>
+                            <input type="file"  class="form-control-file" name="featured">
+                        </div>
+                        <div class="form-group">
+                            <label for="title">Title</label>
+                            <textarea class="form-control" name="content" id="content" cols="5" rows="5"></textarea>                
+                        </div>
+                        <div class="form-group">
+                            <button class="btn btn-success" type="submit">Save</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+        
+@endsection
